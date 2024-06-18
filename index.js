2689fa12cd3db229255b1526947f0ffc84b590d4
André Victor <andrehsvictor@gmail.com>
2024-06-18 20:34:31 -0300

    feat: Add PATCH route for buying a product

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ fastify.get("/products/:id", productService.findById);
 fastify.post("/products", productService.create);
 fastify.put("/products/:id", productService.update);
 fastify.delete("/products/:id", productService.remove);
+fastify.patch("/products/:id/buy", productService.buy);
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Server listening at ${address}`);
+});
 
 module.exports = fastify;
