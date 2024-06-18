4b68b89158922f3c041b51a2d8b2c75ea2c2f3e2
André Victor <andrehsvictor@gmail.com>
2024-06-18 20:34:45 -0300

    chore: Remove server.js file for starting the server

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./index");
-
-app.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  app.log.info(`Server listening at ${address}`);
-});
