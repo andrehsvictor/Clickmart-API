4d1502548dcf97206b260098d0877d2a349e5c4e
André Victor <andrehsvictor@gmail.com>
2024-06-18 19:03:51 -0300

    chore: Add server.js file for starting the server

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./index");
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`Server listening at ${address}`);
+});
